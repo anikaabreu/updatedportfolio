a37368db63c1a855d4ec2c5b633abb60c9c07afe
Anika Abreu <anika.abreu9@gmail.com>
2017-03-24 16:55:05 -0400

    working on shake anims

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#40E0D0',
+    lineColor: '#555555'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
