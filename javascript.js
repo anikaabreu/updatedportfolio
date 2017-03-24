7a460152c4e1c9d2ac094d692dae387d578966d6
Anika Abreu <anika.abreu9@gmail.com>
2017-03-24 14:40:24 -0400

    got particle BG working

--- javascript.js
+++ javascript.js
@@ -1,18 +1,8 @@
-$(".bg").interactive_bg({
-   strength: 25,
-   scale: 1.05,
-   animationSpeed: "100ms",
-   contain: true,
-   wrapContent: false
-   $(document).ready(function(){
-    $(".bg").interactive_bg(); // function call
-});
-
-  // change background size on window resize
-  $(window).resize(function() {
-      $(".bg > .ibg-bg").css({
-        width: $(window).outerWidth(),
-        height: $(window).outerHeight()
-      })
-   })
- });
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
